49079d5dade72887a6ad9b53c9aebc2cf72cebea
Fuzion <fuzionu@gmail.com>
2019-04-21 00:43:25 +0200

    MapRenderer renders borders around viewport

--- src/sex/ui/console/render/MapRenderer.java
+++ src/sex/ui/console/render/MapRenderer.java
@@ -3,26 +3,38 @@ package sex.ui.console.render;
 import sex.game.Board;
 import sex.game.Player;
 
-public class MapRenderer {
+public class MapRenderer
+{
     private static final int VIEWPORT_WIDTH = 50;
     private static final int VIEWPORT_HEIGHT = 15;
 
     private final Player player;
     private final Board map;
 
-    public MapRenderer(Board map, Player player) {
+    public MapRenderer(Board map, Player player)
+    {
         this.player = player;
         this.map = map;
     }
 
-    public String render() {
+    public String render()
+    {
         String map = "";
 
-        for (int i = 0; i < VIEWPORT_HEIGHT; i++) {
-            for (int j = 0; j < VIEWPORT_WIDTH; j++) {
-                map += " ";
+        for (int row = 0; row < VIEWPORT_HEIGHT; row++)
+        {
+            if (row == 0)
+            {
+                map += "┌-----------------------┐\n";
             }
-            map += "\n";
+            else if (row != VIEWPORT_HEIGHT - 1)
+            {
+                map += "│                       │\n";
+            }
+            else
+            {
+                map += "└-----------------------┘\n";
+            }
         }
 
         map += String.format("Player position: (%d, %d)", player.getX(), player.getY());
