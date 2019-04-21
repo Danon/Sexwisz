49e1cf79c573991bb9c889acdf3f076470fd128f
Daniel Wilkowski <danon@noreply.com>
2019-04-21 14:21:10 +0200

    Add Stage

---
+++ src/sex/ui/console/render/map/Stage.java
@@ -0,0 +1,31 @@
+package sex.ui.console.render.map;
+
+import static sex.ui.console.render.map.DimensionalArray.twoDimensionalArray;
+
+class Stage {
+    private final int width, height;
+    private final char[][] map;
+
+    Stage(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.map = twoDimensionalArray(width, height);
+    }
+
+    void clear() {
+        // TODO Kaczor
+        // Napisz funkcje która ustawia wszystkie znaki w tablicy `map` na spacje
+    }
+
+    void put(int x, int y, char character) {
+        // TODO Kaczor
+        // Napisz funkcje która ustawia znak na pozycji `x/y` na znak `character`
+    }
+
+    String render() {
+        // TODO Kaczor
+        // Napisz funkcję która zwraca stringa, który ma wszystkie pola z mapy `map`.
+        // Po każdym wieszu ma być znak nowej linii
+        return "";
+    }
+}
---
+++ test/sex/ui/console/render/map/StageTest.java
@@ -0,0 +1,53 @@
+package sex.ui.console.render.map;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StageTest {
+    @Test
+    void shouldPutCharacters() {
+        // given
+        Stage stage = new Stage(3, 3);
+
+        stage.put(0, 0, '1');
+        stage.put(2, 0, '2');
+        stage.put(0, 2, '3');
+        stage.put(2, 2, '4');
+        stage.put(1, 1, '5');
+
+        // when
+        stage.clear();
+
+        // then
+        assertEquals("1 2\n" + " 5 \n" + "3 4\n", stage.render());
+    }
+
+    @Test
+    void shouldRenderEmpty() {
+        // given
+        Stage stage = new Stage(4, 3);
+
+        // when
+        String result = stage.render();
+
+        // then
+        assertEquals("    \n    \n    \n", result);
+    }
+
+    @Test
+    void shouldClear() {
+        // given
+        Stage stage = new Stage(3, 3);
+
+        stage.put(0, 0, '#');
+        stage.put(2, 2, '#');
+
+        // when
+        stage.clear();
+
+        // then
+        String expected = "   \n   \n   \n";
+        assertEquals(expected, stage.render());
+    }
+}
