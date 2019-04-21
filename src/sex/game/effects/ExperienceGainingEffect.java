c0b19f37d32752eef7a000b30efba871dd94ae43
Daniel Wilkowski <danon@noreply.com>
2019-04-21 02:45:13 +0200

    Add ExperienceGainingEffect.java

---
+++ src/sex/game/effects/ExperienceGainingEffect.java
@@ -0,0 +1,14 @@
+package sex.game.effects;
+
+import sex.game.Player;
+
+public class ExperienceGainingEffect extends Effect {
+    public ExperienceGainingEffect(int duration) {
+        super("Zdobywanie doświadczenia", duration);
+    }
+
+    @Override
+    public void effectContinuous(Player player) {
+        player.gainExperience(1);
+    }
+}
---
+++ src/sex/game/items/consumables/LearningPotion.java
@@ -0,0 +1,21 @@
+package sex.game.items.consumables;
+
+import sex.game.effects.Effect;
+import sex.game.effects.ExperienceGainingEffect;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+
+public class LearningPotion extends EffectiveConsumable {
+    private final int duration;
+
+    public LearningPotion(int duration) {
+        super("Napój uczący");
+        this.duration = duration;
+    }
+
+    public List<Effect> getEffects() {
+        return singletonList(new ExperienceGainingEffect(duration));
+    }
+}
