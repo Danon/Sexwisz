17de21be14c4d07d33668bd0c60b5c74c67907a7
Fuzion <fuzionu@gmail.com>
2019-04-21 18:47:57 +0200

    Implement Stage

--- src/sex/ui/console/render/map/Stage.java
+++ src/sex/ui/console/render/map/Stage.java
@@ -2,35 +2,54 @@ package sex.ui.console.render.map;
 
 import static sex.ui.console.render.map.DimensionalArray.twoDimensionalArray;
 
-class Stage
+public class Stage
 {
     private final int width, height;
-    private final char[][] map;
+    public final char[][] mapStage;
 
-    Stage(int width, int height)
+    public Stage(int width, int height)
     {
         this.width = width;
         this.height = height;
-        this.map = twoDimensionalArray(width, height);
+        this.mapStage = twoDimensionalArray(width, height);
     }
 
     void clear()
     {
-        // TODO Kaczor
-        // Napisz funkcje która ustawia wszystkie znaki w tablicy `map` na spacje
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                mapStage[x][y] = 0;
+            }
+        }
     }
 
     void put(int x, int y, char character)
     {
-        // TODO Kaczor
-        // Napisz funkcje która ustawia znak na pozycji `x/y` na znak `character`
+        mapStage[x][y] = character;
     }
 
-    String render()
+    public String render()
     {
-        // TODO Kaczor
-        // Napisz funkcję która zwraca stringa, który ma wszystkie pola z mapy `map`.
-        // Po każdym wieszu ma być znak nowej linii
-        return "";
+        String result = "";
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                char character = mapStage[x][y];
+                if (character == 0)
+                {
+                    result += ' ';
+                }
+                else
+                {
+                    result += character;
+                }
+            }
+            result += '\n';
+        }
+        return result;
     }
 }
