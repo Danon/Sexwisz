c4f41d94a4780482f10428fa722d85bcaedb1cb7
Daniel Wilkowski <danon@noreply.com>
2019-04-27 19:42:57 +0200

    Add EquipmentRendererTest

---
+++ test/sex/ui/console/render/EquipmentRendererTest.java
@@ -0,0 +1,122 @@
+package sex.ui.console.render;
+
+import org.junit.jupiter.api.Test;
+import sex.game.Equipment;
+import sex.game.items.Item;
+import sex.game.items.Weapon;
+import sex.game.items.consumables.food.Bread;
+import sex.game.items.consumables.food.Water;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EquipmentRendererTest
+{
+    @Test
+    void shouldRenderWater()
+    {
+        // given
+        List<Item> items = List.of(new Water());
+        Equipment equipment = new Equipment(0, items);
+
+        EquipmentRenderer renderer = new EquipmentRenderer(equipment);
+
+        // when
+        String result = renderer.render();
+
+        // then
+        assertEquals(" - Woda (+5 hp)\n", result);
+    }
+
+    @Test
+    void shouldRenderBread()
+    {
+        // given
+        List<Item> items = List.of(new Bread());
+        Equipment equipment = new Equipment(0, items);
+
+        EquipmentRenderer renderer = new EquipmentRenderer(equipment);
+
+        // when
+        String result = renderer.render();
+
+        // then
+        assertEquals(" - Chleb (+45 hp)\n", result);
+    }
+
+    @Test
+    void shouldRenderPhysicalWeapon()
+    {
+        // given
+        List<Item> items = List.of(new Weapon("Miecz", 4, 5, 0, 0));
+        Equipment equipment = new Equipment(0, items);
+
+        EquipmentRenderer renderer = new EquipmentRenderer(equipment);
+
+        // when
+        String result = renderer.render();
+
+        // then
+        assertEquals(" - Broń: Miecz (4-5 fizycznych)\n", result);
+    }
+
+    @Test
+    void shouldRenderMagicalWeapon()
+    {
+        // given
+        List<Item> items = List.of(new Weapon("Różdżka", 0, 0, 4, 5));
+        Equipment equipment = new Equipment(0, items);
+
+        EquipmentRenderer renderer = new EquipmentRenderer(equipment);
+
+        // when
+        String result = renderer.render();
+
+        // then
+        assertEquals(" - Broń: Różdżka (4-5 magicznych)\n", result);
+    }
+
+    @Test
+    void shouldRenderPhysicalAndMagicalWeapon()
+    {
+        // given
+        List<Item> items = List.of(new Weapon("Zatruty sztylet", 2, 4, 0, 2));
+        Equipment equipment = new Equipment(0, items);
+
+        EquipmentRenderer renderer = new EquipmentRenderer(equipment);
+
+        // when
+        String result = renderer.render();
+
+        // then
+        assertEquals(" - Broń: Zatruty sztylet (2-4 fizycznych, 0-2 magicznych)\n", result);
+    }
+
+    @Test
+    void shouldRenderMultiple()
+    {
+        // given
+        List<Item> items = List.of(
+                new Water(),
+                new Bread(),
+                new Weapon("Miecz", 4, 5, 0, 0),
+                new Water(),
+                new Weapon("Topór", 2, 8, 0, 0)
+        );
+        Equipment equipment = new Equipment(15, items);
+
+        EquipmentRenderer renderer = new EquipmentRenderer(equipment);
+
+        // when
+        String result = renderer.render();
+
+        // then
+        String expected = " - Woda (+5 hp)\n" +
+                " - Chleb (+45 hp)\n" +
+                " - Broń: Miecz (4-5 physical)\n" +
+                " - Woda (+5 hp)\n" +
+                " - Broń: Topór (2-8 physical)\n";
+        assertEquals(expected, result);
+    }
+}
