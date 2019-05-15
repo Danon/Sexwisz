7338c059bda216d60352f0d933f9c1b1d13795e9
Daniel Wilkowski <danon@noreply.com>
2019-05-16 00:59:06 +0200

    Refactor

--- src/sex/ui/console/render/ItemsRenderer.java
+++ src/sex/ui/console/render/ItemsRenderer.java
@@ -28,27 +28,34 @@ public class ItemsRenderer
         Item item = items.get(0);
         if (item instanceof Weapon)
         {
-            Weapon weapon = (Weapon) item;
-            if (weapon.attackPhysicalMax() == 0 && weapon.attackMagicalMax() == 0)
-            {
-                return " - Broń: " + item.getName() + " (useless)\n";
-            }
-            if (weapon.attackPhysicalMax() == 0)
-            {
-                return " - Broń: " + item.getName() + " (" + weapon.attackMagicalMin() + "-" + weapon.attackMagicalMax() + " magicznych)\n";
-            }
-            if (weapon.attackMagicalMax() == 0)
-            {
-                return " - Broń: " + item.getName() + " (" + weapon.attackPhysicalMin() + "-" + weapon.attackPhysicalMax() + " fizycznych)\n";
-            }
-            return " - Broń: " + item.getName() + " (" + weapon.attackPhysicalMin() + "-" + weapon.attackPhysicalMax() + " fizycznych, " + weapon.attackMagicalMin() + "-" + weapon.attackMagicalMax() + " magicznych)\n";
+            return renderWeapon((Weapon) item);
         }
         if (item instanceof Food)
         {
-            Food food = (Food) item;
-            return " - " + food.getName() + " (+" + food.healing() + " hp)\n";
+            return renderFood((Food) item);
         }
-
         return "niebroń :(";
+    }
+
+    private String renderWeapon(Weapon weapon)
+    {
+        if (weapon.attackPhysicalMax() == 0 && weapon.attackMagicalMax() == 0)
+        {
+            return " - Broń: " + weapon.getName() + " (useless)\n";
+        }
+        if (weapon.attackPhysicalMax() == 0)
+        {
+            return " - Broń: " + weapon.getName() + " (" + weapon.attackMagicalMin() + "-" + weapon.attackMagicalMax() + " magicznych)\n";
+        }
+        if (weapon.attackMagicalMax() == 0)
+        {
+            return " - Broń: " + weapon.getName() + " (" + weapon.attackPhysicalMin() + "-" + weapon.attackPhysicalMax() + " fizycznych)\n";
+        }
+        return " - Broń: " + weapon.getName() + " (" + weapon.attackPhysicalMin() + "-" + weapon.attackPhysicalMax() + " fizycznych, " + weapon.attackMagicalMin() + "-" + weapon.attackMagicalMax() + " magicznych)\n";
+    }
+
+    private String renderFood(Food item)
+    {
+        return " - " + item.getName() + " (+" + item.healing() + " hp)\n";
     }
 }
