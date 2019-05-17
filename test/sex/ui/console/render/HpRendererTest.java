bfb842fd2ec6b3c9d43bb869b87537345b9994a7
Fuzion <fuzionu@gmail.com>
2019-05-17 02:14:35 +0200

    added HpRendererTest

---
+++ test/sex/ui/console/render/HpRendererTest.java
@@ -0,0 +1,19 @@
+package sex.ui.console.render;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HpRendererTest
+{
+    @Test
+    void shouldRenderHP
+    {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
