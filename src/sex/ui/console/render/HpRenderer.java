104fe336aceabe3a9e038871f0d42e9353963ef6
Fuzion <fuzionu@gmail.com>
2019-05-20 00:50:14 +0200

    class HpRenderer is done, all test pass

--- src/sex/ui/console/render/HpRenderer.java
+++ src/sex/ui/console/render/HpRenderer.java
@@ -1,9 +1,31 @@
 package sex.ui.console.render;
+import java.lang.IllegalArgumentException;
 
 public class HpRenderer
 {
     public String render(int actual, int max)
     {
-        return "";
+        String CurrentHP = "|";
+        for (int x = 0; x < actual; x++)
+        {
+            CurrentHP += "X";
+        }
+
+
+        if (actual < max)
+        {
+            int actMaxDiff = max - actual;
+            for (int z = 0; z < actMaxDiff; z++)
+            {
+                CurrentHP += "-";
+            }
+        }
+
+        if (actual < 0 || max < 0 || actual > max )
+        {
+            throw new IllegalArgumentException();
+        }
+
+        return CurrentHP + "|";
     }
 }
