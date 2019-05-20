f5e412b0c775df5fc60d01a8af40998aaab95692
Fuzion <fuzionu@gmail.com>
2019-05-21 00:58:29 +0200

    add ProportionalHpRenderer

--- src/sex/ui/console/render/ProportionalHpRenderer.java
+++ src/sex/ui/console/render/ProportionalHpRenderer.java
@@ -2,13 +2,23 @@ package sex.ui.console.render;
 
 public class ProportionalHpRenderer
 {
+    private final int width;
+    private final HpRenderer hpRenderer;
+
     ProportionalHpRenderer(int width)
     {
+        this.hpRenderer = new HpRenderer();
+        this.width = width;
 
+        if (width < 2)
+        {
+            throw new IllegalArgumentException();
+        }
     }
 
     public String render(int actual, int max)
     {
-        return "";
+        double ratio = Math.round((double) actual / max * width);
+        return hpRenderer.render((int) ratio, width);
     }
 }
